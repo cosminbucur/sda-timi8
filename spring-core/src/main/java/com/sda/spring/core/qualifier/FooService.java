78caa13e470123dadd2f56eefb57005315c78cce
cosmin_bucur <cosmindanbucur@gmail.com>
2020-09-12 14:43:08 +0300

    add work 12.09.2020

---
+++ spring-core/src/main/java/com/sda/spring/core/qualifier/FooService.java
@@ -0,0 +1,18 @@
+package com.sda.spring.core.qualifier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FooService {
+
+    // delegate to formatter
+    @Autowired
+    @Qualifier("barFormatter")
+    private Formatter formatter;
+
+    public void run() {
+        formatter.format("hello");
+    }
+}
